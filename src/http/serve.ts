596d2333a9190760684afac6da32a34355198f72
gabrielrcosta1 <gabrielribeiro@grsoftwares.com.br>
2024-02-06 08:51:23 -0400

    created docker image for redis and postgres

--- src/http/serve.ts
+++ src/http/serve.ts
@@ -3,6 +3,10 @@ import fastify from 'fastify'
 const app = fastify();
 
 
+app.get('/hello',()=>{
+    return "Hello Word"
+})
+
 app.listen({port: 3333}).then(()=>{
     console.log('HTTP serve running! ')
 })
