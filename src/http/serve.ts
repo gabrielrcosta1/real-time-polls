6a6a6018e13fcca3b4f8326a201793139422cb29
gabrielrcosta1 <gabrielribeiro@grsoftwares.com.br>
2024-02-06 12:00:24 -0400

    organizing files in their folders, and creating options for polls

--- src/http/serve.ts
+++ src/http/serve.ts
@@ -1,12 +1,9 @@
 import fastify from 'fastify'
+import { createPoll } from './routes/create-poll';
 
 const app = fastify();
+app.register(createPoll)
 
-
-app.get('/hello',()=>{
-    return "Hello Word"
-})
-
-app.listen({port: 3333}).then(()=>{
+app.listen({ port: 3333 }).then(() => {
     console.log('HTTP serve running! ')
 })
